3b8c26b73daf7198e890403e3dbd708a999f98f7
Airmid <forsa9828@gmail.com>
2020-04-18 21:14:34 -0500

    set up index.js

---
+++ clickygame/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Components/nav/index';
+
+
+function App() {
+  return (
+    <div className="App">
+     <p> Alicia</p>
+    <Nav/>
+    </div>
+  );
+}
+
+export default App;
